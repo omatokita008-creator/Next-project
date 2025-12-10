e56526934e7241c096f60984230a53fb45b3dd04
omatokita008-creator <omatokita008@gmail.com>
2025-12-10 13:38:25 +0100

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "My App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
